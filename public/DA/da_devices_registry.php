905903c5929b17be7cccb98c39b5e6b14e64172a
Jose Nunez <jose.nunez@intel.com>
2014-12-06 18:54:05 -0600

    basic Data Access Layer for devices

---
+++ public/DA/da_devices_registry.php
@@ -0,0 +1,195 @@
+<?php
+
+/**
+ * Description of da_devices_registry
+ *
+ * @author janunezc
+ */
+class be_device {
+
+    public $device_id = 0;
+    public $account_id = 0;
+    public $device_nickname = "";
+    public $device_description = "";
+    public $api_key = "";
+    public $created_datetime = NULL;
+    public $modified_datetime = NULL;
+    public $deleted_datetime = NULL;
+    public $last_connected_datetime = NULL;
+
+}
+
+class da_devices_registry {
+
+    /**
+     * Registers a device and returns the resultant record as be_device
+     * @param be_device $device
+     * @return type
+     */
+    public static function RegisterNewDevice($device) {
+        $sqlCommand = "INSERT INTO device_registry (account_id,device_nickname,device_description,api_key)"
+                . "VALUES (?,?,?,SHA1(UUID()))";
+
+        $paramTypeSpec = "sss";
+
+        $mysqli = DA_Helper::mysqli_connect();
+        if ($mysqli->connect_errno) {
+            $msg = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!($stmt = $mysqli->prepare($sqlCommand))) {
+            $msg = "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!$stmt->bind_param($paramTypeSpec, $device->account_id, $device->device_nickname, $device->device_description)) {
+            $msg = "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!$stmt->execute()) {
+            $msg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        $stmt->close();
+
+        $insertedDeviceID = $mysqli->insert_id;
+
+        $retrievedDevice = da_devices_registry::GetDevice($insertedDeviceID);
+        return $retrievedDevice;
+    }
+
+    /**
+     * Returns a device found by its ID
+     * @param int $device_id
+     * @return be_device
+     */
+    public static function GetDevice($device_id) {
+        $sqlCommand = ""
+                . "SELECT   device_id,
+                            account_id,
+                            device_nickname,
+                            device_description,
+                            api_key,
+                            created_datetime,
+                            modified_datetime,
+                            deleted_datetime,
+                            last_connected_datetime"
+                . " FROM device_registry "
+                . " WHERE device_id=? ";
+
+        $mysqli = DA_Helper::mysqli_connect();
+        if ($mysqli->connect_errno) {
+            $msg = "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!($stmt = $mysqli->prepare($sqlCommand))) {
+            $msg = "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!$stmt->bind_param("i", $device_id)) {
+            $msg = "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        if (!$stmt->execute()) {
+            $msg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            throw new Exception($msg, $stmt->errno);
+        }
+
+        $result = new be_device();
+        $stmt->bind_result(
+                $result->device_id, $result->account_id, $result->device_nickname, $result->device_description, $result->api_key, $result->created_datetime, $result->modified_datetime, $result->deleted_datetime, $result->last_connected_datetime
+        );
+
+        if (!$stmt->fetch()) {
+            $result = NULL;
+        }
+
+        $stmt->close();
+
+        return $result;
+    }
+
+    /**
+     * Returns a list of devices for a given user account
+     * @param type $account_id
+     * return Array
+     */
+    public static function GetListOfDevices($account_id) {
+        $sqlCommand = 
+                "SELECT     device_id,
+                            account_id,
+                            device_nickname,
+                            device_description,
+                            api_key,
+                            created_datetime,
+                            modified_datetime,
+                            deleted_datetime,
+                            last_connected_datetime "
+                . "FROM device_registry "
+                . "WHERE account_id = ?";
+
+        $mysqli = DA_Helper::mysqli_connect();
+        if ($mysqli->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        }
+
+        if (!($stmt = $mysqli->prepare($sqlCommand))) {
+            echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        }
+
+        if (!$stmt->bind_param("i", $account_id)) {
+            echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+        }
+
+        $deviceEntry = new be_device();
+
+        $stmt->bind_result(
+                $deviceEntry->device_id,
+                $deviceEntry->account_id,
+                $deviceEntry->device_nickname,
+                $deviceEntry->device_description,
+                $deviceEntry->api_key,
+                $deviceEntry->created_datetime,
+                $deviceEntry->modified_datetime,
+                $deviceEntry->deleted_datetime,
+                $deviceEntry->last_connected_datetime);
+
+        $arrayResult = [];
+        while ($stmt->fetch()) {
+            $arrayResult[] = json_decode(json_encode($deviceEntry));
+        }
+
+        $stmt->close();
+
+        return $arrayResult;
+    }
+
+    /**
+     * Updates a device with the provided information and returns the resulting record as saved.
+     * @param be_device $device
+     * @return be_device
+     */
+    public static function UpdateDevice($device) {
+        return $device;
+    }
+
+    /**
+     * Deletes a device by updating its deleted_datetime
+     * @param be_device $device
+     * @return be_device
+     */
+    public static function DeleteDevice($device) {
+        
+    }
+
+}
--- public/test.php
+++ public/test.php
@@ -3,6 +3,7 @@ require_once './DA/da_conf.php';
 require_once './DA/da_helper.php';
 require_once './DA/da_account.php';
 require_once './DA/da_session.php';
+require_once './DA/da_devices_registry.php';
 ?>
 
 <!DOCTYPE html>
@@ -11,18 +12,67 @@ require_once './DA/da_session.php';
         <meta charset="UTF-8">
         <title>AUTOMATED TEST FILE</title>
     </head>
-    <body>
+    <body style="font-family:courier">
         <?php
+        ReportInfo("Initiating Tests!");
+
         test_da_session();
+        test_da_device();
+
+
+        ReportInfo("Tests Finished!");
         ?> 
     </body>
 </html>
 
 <?php
 
+function test_da_device() {
+    $testName = "DA DEVICE TEST";
+    ReportInfo("Initiating $testName");
+
+    $newDevice = new be_device();
+    $newDevice->account_id = 1;
+    $newDevice->device_nickname = "JN GALILEO1";
+    $newDevice->device_description = "";
+
+    ReportInfo("Device to Create:");
+    print_r($newDevice);
+
+    $registeredDevice = da_devices_registry::RegisterNewDevice($newDevice);
+    ReportInfo("Device Created:");
+    print_r($registeredDevice);
+
+    if ($registeredDevice->device_id > 0) {
+        ReportSuccess("Created Device seems to be OK!");
+    } else {
+        ReportError("Created device seems to be WRONG! :(");
+    }
+
+    ReportInfo("Testing retrieval of a list of devices for an account...");
+    $account_id = 1;
+    $devices = da_devices_registry::GetListOfDevices($account_id);
+    print_r($devices);
+    if (count($devices)>0){
+        ReportSuccess("Result seems to be fine.");
+    } else {
+        ReportSuccess("Result seems to be WRONG");
+    }
+    
+    ReportInfo("Complete: $testName");
+}
+
 function test_da_session() {
+    ReportInfo("Initiating Session Test");
+    ReportInfo("creating session on jose.a.nunez@gmail.com");
     $session = da_session::CreateSession("jose.a.nunez@gmail.com");
     print_r($session);
+    if ($session->token != '') {
+        ReportSuccess("Session seems to be Correct!");
+    } else {
+        ReportError("Session seems to be BAD");
+    }
+    ReportInfo("Session Tests Complete!");
 }
 
 function test_da_account() {
@@ -34,4 +84,18 @@ function test_da_account() {
         print_r($createdAccount);
     }
 }
-?>
+
+function ReportInfo($message) {
+    $moment = Date("Y-m-d H:i:s");
+    echo("<hr><div style=\"color:darkblue\">$moment - $message</div>");
+}
+
+function ReportSuccess($message) {
+    $moment = Date("Y-m-d H:i:s");
+    echo("<hr><div style=\"color:green;\">$moment - $message</div>");
+}
+
+function ReportError($message) {
+    $moment = Date("Y-m-d H:i:s");
+    echo("<hr><div style=\"color:red;\">[!!!]$moment - $message</div>");
+}
