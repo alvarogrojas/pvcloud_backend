bc3d56b4af7f76237af4f92631d056aa3606cfd2
Oscar Rodriguez Arroyo <oscar.rodriguez.arroyo@intel.com>
2015-03-26 16:38:44 -0600

    app visibility

--- public/inc/incWebServiceAPIKeyValidation.php
+++ public/inc/incWebServiceAPIKeyValidation.php
@@ -4,13 +4,21 @@ $account_id = filter_input(INPUT_GET, "account_id");
 $app_id  = filter_input(INPUT_GET, "app_id");
 $api_key = filter_input(INPUT_GET, "api_key");
 
-if(!isset($app_id) || !isset($account_id) || !isset($api_key)){
+if(!isset($apikey)){
+	$api_key="";
+}
+
+if(!isset($app_id) || !isset($account_id)){
     die();
 }
 
 $app = da_apps_registry::GetApp($app_id);
 
-$validation = $account_id==$app->account_id && $api_key == $app->api_key;
+if($app->visibility_type_id == 3){ // 3 = Public App 
+	$validation = $account_id==$app->account_id; 
+}else{
+	$validation = $account_id==$app->account_id && $api_key == $app->api_key;
+}
 
 if(! $validation) {
     die();
